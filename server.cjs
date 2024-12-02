7598bd1efdb225bfc0fc6a7c6af2be42101b421a
Nazarii Lesiuk <lesiuk.nazarii@growe.com>
2024-12-02 18:22:31 +0200

    cors

--- server.cjs
+++ server.cjs
@@ -8,10 +8,13 @@ const delay = (ms = 1000) => new Promise((res, rej) => {
   setTimeout(res, ms)
 })
 
+app.use(cors())
+
 // routes
-const wifi = '/wifi'
-router.get(wifi, async (req, res) => {
-  await delay(1000)
+const wifi = 'wifi'
+router.get(wifi, (req, res) => {
+  console.log('req1', req.url)
+  // await delay()
   res.status(200).send([
     { id: 0, ssid: 'ssid-0' },
     { id: 1, ssid: 'ssid-1' },
@@ -19,17 +22,18 @@ router.get(wifi, async (req, res) => {
     { id: 3, ssid: 'ssid-3' },
   ]);
 });
-
+// routes registration
+app.use('/api/v1', router);
 // routes error handler
 app.use((req, res) => {
+  console.log('req', req.url)
   res.status(404).send({ message: 'Not Found' });
 });
 
-// routes registration
-app.use('/api/v1', router);
 
 
-app.use(cors())
+
+
 // start server
 const port = 4000
 const server = app.listen(port);
